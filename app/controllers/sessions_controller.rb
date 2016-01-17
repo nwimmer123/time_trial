d387ecdf9cc0bbe5a11a532e16153d26333699de
Noah Wimmer <nwimmer123@yahoo.com>
2016-01-16 19:43:59 -0800

    added create and destrot to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,18 @@ class SessionsController < ApplicationController
 	end
 
 	def create
+		user = User.find_by_email(params[:email])
+		if user && user.authentication(params[:password])
+			session[:user_id] = user.id
+			redirect to '/'
+		else
+			redirect_to '/login'
+		end
 	end
 
 	def destroy
+		session[:user_id] = nil
+		redirect_to '/login'
 	end
-	
+
 end
